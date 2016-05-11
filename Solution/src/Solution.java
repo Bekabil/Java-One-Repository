6210c0eae199903c6caa7f5d43a49b18d4231792
Bekabil <bekabil.tolassa@gmail.com>
2016-05-10 22:16:41 -0600

    comments are added to class Solution

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,6 +1,20 @@
 import java.io.*;
 import java.util.*;
+/**
+ * 
+ * @author Bekabil Tolassa
+ * 
+ * Given an array of N integers, this program finds the sum of its elements?
 
+ * Input Format
+
+ * The first input is an integer, N, denoting the size of the array.
+ * The second input is an N space-separated integers representing the array's elements.
+
+ * Output Format
+ * This program print the sum of the array's elements as a single integer. 
+ *
+ */
 public class Solution {
 
     public static void main(String[] args) {
@@ -9,11 +23,11 @@ public class Solution {
         int size = 0; 
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Enter array size: ");
+        System.out.print("Enter an array size: ");
         size = input.nextInt();
         
         int[] myList = new int[size];
-        System.out.print("Enter a number: ");
+        System.out.print("Enter " + size + " numbers to be added: ");
         for(int i = 0; i < myList.length; i++) {
             
             myList[i] = input.nextInt();
@@ -22,6 +36,6 @@ public class Solution {
         for(int i = 0; i < myList.length; i++) {
             sum = sum + myList[i];
         }
-        System.out.print(sum);
+        System.out.print("The sum is " + sum);
     }
 }
