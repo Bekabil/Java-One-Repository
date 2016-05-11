7996be5573758f421f907273a3a9611391157594
Bekabil <bekabil.tolassa@gmail.com>
2016-05-10 23:19:43 -0600

    Comments are added to a class Precition.

--- Solution/src/Precition.java
+++ Solution/src/Precition.java
@@ -1,4 +1,25 @@
+/**
+ * 
+ * @author Bekabil Tolassa
+ * 
+ * Given an array of integers, this program calculates which fraction of its elements are positive, 
+ * which fraction of its elements are negative, and which fraction of its elements are zeroes, respectively. 
+ * It prints the decimal value of each fraction on a new line.
+ * This program also prints the output with precision specific. 
+ * The test cases are scaled to six decimal places. 
 
+ * Input Format
+
+ * The first line of input contains an integer, N, denoting the size of the array.
+ * The second line contains N space-separated integers describing an array of numbers.
+
+ * Output Format
+
+ * A decimal representing of the fraction of positive numbers in the array.
+ * A decimal representing of the fraction of negative numbers in the array.
+ * A decimal representing of the fraction of zeroes in the array.
+ *
+ */
 public class Precition {
 
 	private static int size;
