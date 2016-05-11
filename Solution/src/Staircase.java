e8244523127360bd0723292e27ec0d374a7bc478
Bekabil <bekabil.tolassa@gmail.com>
2016-05-10 22:35:34 -0600

    Comments are added to a class Staircase

--- Solution/src/Staircase.java
+++ Solution/src/Staircase.java
@@ -1,8 +1,20 @@
-
+/**
+ * 
+ * @author Bekabil Tolassa
+ * 
+ * Given the required height, this program print a staircase.
+ *
+ * Input
+ * You are given an integer of N depicting the height of the staircase.
+ *
+ * Output
+ * Print a staircase of height N that consists of # symbols and spaces. 
+ *
+ */
 public class Staircase {
 
 	public static void main(String[] args) {
-		int max = 6;
+		int max = 10;
 		int num = max;
 		int min = 1;
 		int count = 0;
